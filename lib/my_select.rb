14d9780938eaa87fb2db1a664e2e834b96c9d235
jodyneckles <jody@technative.tv>
2018-11-10 14:13:03 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
+
+nums = [1, 2, 3, 4, 5]
 def my_select(collection)
- # your code here!
-end
+   i = 0
+   selected = []
+   while i < collection.length
+     if yield(collection[i])
+       selected << collection[i]
+     end
+     i = i + 1
+   end
+   selected
+  end
+
+my_select(nums){|number| number.even?}
